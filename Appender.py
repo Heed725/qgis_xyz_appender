b89939454615c191b1986c06cee42302eb847b8d
Hemed Lungo <86722789+Heed725@users.noreply.github.com>
2026-03-12 15:05:08 +0300

    Create Appender.py

---
+++ Appender.py
@@ -0,0 +1,75 @@
+xyzLayers = []
+
+# 2017
+xyzLayers.append(["Wami True_Color_2017", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/779d36d330915029e7e8d9732ddd921a-aa4a37db2ea96447d743389d7d9ca3c3/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami False_Color_2017", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/637ea579acf359e113cb482c02045a2d-039c14e8d6f849361753310004480225/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDVI_2017", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/d45b00fb800b8e65c33b352b2d02b59c-9fa987de9dc0760be1d32a4315936bb9/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDWI_2017", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/357604728fc9daa79a9da51ae9c9bad2-19dbb11fbd49c1e533267ea8816105b8/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Pre_Flood_True_Color_2017", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/cb9d8cc024294faf9ad8f30a95c7896a-dd29cd03786420a90d715f255be4a8aa/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami MNDWI_2017", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/17914a70471f9bd541cb2673ffcd9378-4c87f5e7422ef2b12f88d22f5598973a/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Post_Flood_True_Color_2017", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/9408914199e4d466e7b1f096558792e3-8ee0bd54d5f2f9862215d21dd94a9ba3/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+
+# 2018
+xyzLayers.append(["Wami True_Color_2018", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/286cfb4f9a3533ab3dd27c970afd6ea0-e36beff184d0eae897d9d5e64a46e079/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami False_Color_2018", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/00254713e740686d582a900ba7f966bd-0e5e22be0aee57d289aa1bcb79cb7958/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDVI_2018", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/e876a22d4629fb1f6ca00026b4eccb03-b1a7bdaab304231178d96893e97ddc7e/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDWI_2018", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/5b7b17668167f5b6dfbea3e7ae517309-e8135ad40bd221d415a249b0a40883d1/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami MNDWI_2018", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/b1f37693a7def425be8c2ece0a94f372-ea5cd89553ca2f0113a9b996191599b5/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Pre_Flood_True_Color_2018", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/7f6875db00b5c7c575da1b8f90b6ab85-9896f2d964cd8fcd8baa162e990d338f/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Post_Flood_True_Color_2018", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/56e68d3d7e39ac8fc905ce6bcc756ed0-3d07e6aee1d4149abc2601e4ec628ae5/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+
+# 2021
+xyzLayers.append(["Wami True_Color_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/c2c77ec63fd2632455c036cba83876d1-6e74cc5a8030ced0df5c76b46421637f/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami False_Color_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/a9bfe477e480314ae4f1042ab163089b-3ef11c5104daad19f440aac9d46cd777/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Pre_Flood_True_Color_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/82f4f0895b8ca805ddf7c35647a0b9b1-f352e62520d66f7200e653ff8d23dac4/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Post_Flood_True_Color_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/3b22601e9173ede1440924adc192f958-b51b52c67cc33bed0eadf17926848b6f/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami MNDWI_Difference_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/70b3a54edcf403f688c91a54c4d9d3b1-8ab8244426ba37647b526997fdc4fdf9/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDVI_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/b1c0f4b1f39c3f388cdff73b58431c7b-2d231058020bb5252600e96d01b3b1fc/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_MNDWI_Threshold_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/a9c4ff5427018120cf8388d0654af3e8-14c6751a2a1b7153d170c164835af9c8/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_MNDWI_Difference_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/34a6bc4d51989a66085e469e0e0711db-fa583e320e18a619af9d01150e8e2f96/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDWI_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/4eef4e45522f818ad9e4485d3c43d0cf-73a6d209e2592f2967edef3c6338c362/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_Classification_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/2b1a53ee2d4e348c557d06e9d53cdce5-958b47ae552659a9bee516f592894686/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami MNDWI_2021", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/6ad5f1e56ae496e898ff474743fdb16e-20c656cab743e89bf166f115e8e5fab6/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+
+# 2022
+xyzLayers.append(["Wami True_Color_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/c82788ce08f754eddbdabe0d8ec11a74-436cfc0dda2865063e5c01cbc0c5feda/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami False_Color_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/438dcb17c09913a4e01e34b040eaaa25-f10bf18ee1e4be07c33eeb5208b53ad0/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDVI_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/80ab2a8ff1883a7c72d1c66a9b025336-e8bad9d4465b3e4b9f125fbacad1de48/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Pre_Flood_True_Color_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/3265d3b15c57d028dbaeb2a3c26530e8-bbb796b89eff8f7327ac14c0070e8a3c/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Post_Flood_True_Color_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/227b9d2fa7f1b9cb3d1e57824bcdc434-708ba0ec55a04a8f2a0109c4f06491f4/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami MNDWI_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/2b0a69bff14dc246e765f8ed54f691ac-2cca9083a66b8e23496692c1e7ed33c0/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDWI_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/ee172003fbbc7fc901662158ee487e44-4253c48caa6215bef5b0f4c6370dbb7c/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami MNDWI_Difference_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/0cd164e49416bb86334b369cc2b9d703-9aaf6e01ea6d5886ca71d4b30b87146f/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_MNDWI_Difference_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/f1a38240d96bd40b1d9cdb2f3abfe262-c511385379a362ed5ddb43b8b276ed8c/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_MNDWI_Threshold_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/090e02b0b0b94e9a665c2a0fc2a3cb36-4f5f047e8122f564c32e474e4d44786e/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_Classification_2022", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/43d072bd7f72c904ede34c4fb1571742-3bdb66e4628552f20bba8de5da54953f/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+
+# 2025
+xyzLayers.append(["Wami True_Color_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/b2ce37cb86e509a4e04c851b4fcbe244-4be889dcb756f26a78c72559b240af76/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Pre_Flood_True_Color_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/af23832be73caca1f5792e5a7c85deb7-eb57d24e4903b977551abac2f38e4c09/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami False_Color_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/b2c3580407b42d39eefc2a385eb18b94-f9bfd378b2765a3d403e403499c4b4e6/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami MNDWI_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/db59ba7feb2aed45fa97d01095f85ab2-dbe9bc622785d37f199763c0da6cf255/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDWI_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/dd4f62bd3e0a166a3c96c62694fc59a9-a64c8d1c05f00afce5136bf23d8b79e3/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami NDVI_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/c165fb9f0bd8ee9844af5a072d79fe26-284970e0b15ee9289a1a850e1e03b053/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Post_Flood_True_Color_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/d5f4f4f432d4c9a10f95a641bc3fcc56-b8dfc68be5b5af2bc1c4732c5844ea74/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_Classification_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/d8a768990ddf4d84f3872c9e305e6cc1-32b3b041ebf1c4f9f89c908e9c96dac0/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_MNDWI_Threshold_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/4c8146218af2f3588b5494dda313e036-065a3cb7f6596bc8a5db64e81b9a8e75/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami Flood_MNDWI_Difference_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/ee1b2fa39c2c5fb1f84db1227aab7d49-5fff7fd1150897ada31f3cbfe3d87eec/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+xyzLayers.append(["Wami MNDWI_Difference_2025", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/536689aa9000bbcdaf2b0d19bfbc6953-43fbbf551d63fea3e0c19a5163095aad/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+
+# Static
+xyzLayers.append(["Wami Permanent_Water_JRC", "https://earthengine.googleapis.com/v1/projects/ee-hemedlungo/maps/9b61fc06a5813ffdae65129b30108df9-dfd9334e01687/tiles/%7Bz%7D/%7Bx%7D/%7By%7D", "19", "0"])
+
+# Register all layers
+for layer in xyzLayers:
+    name, url, zmax, zmin = layer
+    QSettings().setValue("qgis/connections-xyz/%s/url" % (name), url)
+    QSettings().setValue("qgis/connections-xyz/%s/zmax" % (name), zmax)
+    QSettings().setValue("qgis/connections-xyz/%s/zmin" % (name), zmin)
+    QSettings().setValue("qgis/connections-xyz/%s/authcfg" % (name), "")
+    QSettings().setValue("qgis/connections-xyz/%s/username" % (name), "")
+    QSettings().setValue("qgis/connections-xyz/%s/password" % (name), "")
+    QSettings().setValue("qgis/connections-xyz/%s/referer" % (name), "")
+
+iface.reloadConnections()
+print(f"Done! {len(xyzLayers)} Wami layers registered.")
